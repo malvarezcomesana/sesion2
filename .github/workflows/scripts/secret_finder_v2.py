5380279fb6366ade236287eccc2869a1d357ac2f
malvarezcomesana <malvarezcomesana@danielcastelao.org>
2024-11-05 17:09:46 +0100

    Subidaç

--- .github/workflows/scripts/secret_finder_v2.py
+++ .github/workflows/scripts/secret_finder_v2.py
@@ -5,11 +5,19 @@
 # Tip 1: Comando egrep de linux, método walk de la librería os, etc.
 # Tip 2: Códigos de salida con error para github
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/setting-exit-codes-for-actions
-
-
+import sys
+import os
+import subprocess
+from unittest import removeResult
 def main():
-    
-    print("Todavía no estoy listo!")
-
+    ruta = sys.argv[1]
+    clave = sys.argv[2]
+    result = subprocess.run(['egrep', '-R', clave,ruta], capture_output=True, text=True)
+    print(result)
+    if (result.returncode == 0):
+        sys.exit(1)
 if __name__ == "__main__":
+    if len(sys.argv)!=3:
+        print("Uso: python secret_finder_v2.py <ruta> <clave>")
+        sys.exit(1)
     main()
